5794a73947ff1e396fa3a4108e3d9c38eb6eb230
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2020-08-30 11:19:58 +0300

    Add files via upload

---
+++ Programming Basics with C#/F01. First steps in coding/1. Lab/First-Steps-in-Coding-Lab/L03.Nums1To10/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L03.Nums1To10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 10; num++)
+            {
+                Console.WriteLine(num);
+            }
+
+            //other example
+            /*  Console.WriteLine(1);
+                Console.WriteLine(2);
+                Console.WriteLine(3);
+                Console.WriteLine(4);
+                Console.WriteLine(5);
+                Console.WriteLine(6);
+                Console.WriteLine(7);
+                Console.WriteLine(8);
+                Console.WriteLine(9);
+                Console.WriteLine(10);  */
+        }
+    }
+}
