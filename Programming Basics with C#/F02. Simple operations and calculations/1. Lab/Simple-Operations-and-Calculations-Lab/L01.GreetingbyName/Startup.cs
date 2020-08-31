ccbb60d9bd481a86eedfe419f02b0faeefa58fe6
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2020-08-31 15:13:42 +0300

    Add files via upload

---
+++ Programming Basics with C#/F02. Simple operations and calculations/1. Lab/Simple-Operations-and-Calculations-Lab/L01.GreetingbyName/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace L01.GreetingbyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            //first way
+            Console.WriteLine($"Hello, {name}!");
+            //second way
+            Console.WriteLine("Hello, " +name+ "!");
+            //third way
+            Console.WriteLine("Hello, {0}!",  name);
+        }
+    }
+}
---
+++ Programming Basics with C#/F02. Simple operations and calculations/1. Lab/Simple-Operations-and-Calculations-Lab/L03.SquareArea/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace L03.SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            int squareArea = side * side;
+            Console.WriteLine(squareArea);
+        }
+    }
+}
