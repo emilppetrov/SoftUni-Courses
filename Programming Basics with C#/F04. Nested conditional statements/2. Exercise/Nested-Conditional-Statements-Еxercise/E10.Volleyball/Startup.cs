ad14324f4ed3d35f7f660ca1ccf207d5dbcb86d9
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2020-09-02 19:04:07 +0300

    Add files via upload

---
+++ Programming Basics with C#/F04. Nested conditional statements/2. Exercise/Nested-Conditional-Statements-Еxercise/E10.Volleyball/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace E10.Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine().ToLower();
+            int holidays = int.Parse(Console.ReadLine());
+            int weekends = int.Parse(Console.ReadLine());
+            double sweekends = 48 - weekends;
+            double playdays = (sweekends * 3.0 / 4) + (holidays * 2.0 / 3) + weekends;
+            if (year == "normal")
+            {
+                Console.WriteLine($"{Math.Floor(playdays)}:F2");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Floor(playdays * 0.15 + playdays)}:F2");
+            }
+        }
+    }
+}
