447779177e00839249507af7a227f780703673fc
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 11:51:51 +0200

    Add files via upload

---
+++ Programming Basics with C#/F06. For-loops/1. Lab/For-loops-Lab/L06.VowelsSum/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L06.VowelsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'a': sum += 1; break;
+                    case 'e': sum += 2; break;
+                    case 'i': sum += 3; break;
+                    case 'o': sum += 4; break;
+                    case 'u': sum += 5; break;
+                    case 'y': sum += 6; break;
+                }
+            }
+
+            Console.WriteLine("Vowels sum = " + sum);
+
+        }
+    }
+}
---
+++ Programming Basics with C#/F06. For-loops/1. Lab/For-loops-Lab/L09.LeftandRightSum/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace L09.LeftandRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                leftSum += currentNumber;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                rightSum += currentNumber;
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                int difference = Math.Abs(leftSum - rightSum);
+                Console.WriteLine($"No, diff = {difference}");
+            }
+        }
+    }
+}
