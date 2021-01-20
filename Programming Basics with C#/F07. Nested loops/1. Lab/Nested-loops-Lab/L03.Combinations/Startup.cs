16f7ab466116121ac7fda240914f3b76c9d579ba
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 11:41:25 +0200

    Add files via upload

---
+++ Programming Basics with C#/F07. Nested loops/1. Lab/Nested-loops-Lab/L03.Combinations/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace L03.Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int firstNumber = 0; firstNumber <= n; firstNumber++)
+            {
+                for (int secondNumber = 0; secondNumber <= n; secondNumber++)
+                {
+                    for (int thirdNumber = 0; thirdNumber <= n; thirdNumber++)
+                    {
+
+                        int sum = firstNumber + secondNumber + thirdNumber;
+                        if (sum == n)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Programming Basics with C#/F07. Nested loops/1. Lab/Nested-loops-Lab/L04.SumofTwoNumbers/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace L04.SumofTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = int.Parse(Console.ReadLine());
+            int finalNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int combinations = 0;
+            bool isFound = false;
+            for (int i = startingNumber; i <= finalNumber; i++)
+            {
+                for (int j = startingNumber; j <= finalNumber; j++)
+                {
+                    combinations++;
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {magicNumber})");
+                        isFound = true;
+                        return;
+                    }
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
