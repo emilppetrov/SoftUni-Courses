87b1bd42f88615842c1ecd158eeda386639a39b9
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 11:55:24 +0200

    Add files via upload

---
+++ Programming Basics with C#/F06. For-loops/2. Exercise/For-loops-Exercise/E04.Histogram/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace E04.Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int counter1 = 0;
+            int counter2 = 0;
+            int counter3 = 0;
+            int counter4 = 0;
+            int counter5 = 0;
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num < 200)
+                {
+                    counter1++;
+                }
+                else if (num >= 200 && num <= 399)
+                {
+                    counter2++;
+                }
+                else if (num >= 400 && num <= 599)
+                {
+                    counter3++;
+                }
+                else if (num >= 600 && num <= 799)
+                {
+                    counter4++;
+                }
+                else if (num >= 800)
+                {
+                    counter5++;
+                }
+            }
+
+            double p1 = (counter1 * 1.0 / numbers) * 100;
+            double p2 = (counter2 * 1.0 / numbers) * 100;
+            double p3 = (counter3 * 1.0 / numbers) * 100;
+            double p4 = (counter4 * 1.0 / numbers) * 100;
+            double p5 = (counter5 * 1.0 / numbers) * 100;
+
+            Console.WriteLine($"{p1:F2}%");
+            Console.WriteLine($"{p2:F2}%");
+            Console.WriteLine($"{p3:F2}%");
+            Console.WriteLine($"{p4:F2}%");
+            Console.WriteLine($"{p5:F2}%");
+        }
+    }
+}
---
+++ Programming Basics with C#/F06. For-loops/2. Exercise/For-loops-Exercise/E05.DivideWithoutRemainder/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace E05.DivideWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counterDivideBy1 = 0;
+            int counterDivideBy2 = 0;
+            int counterDivideBy3 = 0;
+            int counterDivideBy4 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    counterDivideBy2++;
+                }
+
+                if (number % 3 == 0)
+                {
+                    counterDivideBy3++;
+                }
+
+                if (number % 4 == 0)
+                {
+                    counterDivideBy4++;
+                }
+            }
+
+            double percentDivideBy2 = (counterDivideBy2 * 1.0 / n) * 100;
+            double percentDivideBy3 = (counterDivideBy3 * 1.0 / n) * 100;
+            double percentDivideBy4 = (counterDivideBy4 * 1.0 / n) * 100;
+
+            Console.WriteLine($"{percentDivideBy2:F2}%");
+            Console.WriteLine($"{percentDivideBy3:F2}%");
+            Console.WriteLine($"{percentDivideBy4:F2}%");
+        }
+    }
+}
