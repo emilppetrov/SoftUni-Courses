b02b16f3f13fc150bcfe75a39e1402cfed1be747
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 11:38:55 +0200

    Add files via upload

---
+++ Programming Basics with C#/F06. For-loops/2. Exercise/For-loops-Exercise/E02.HalfSumElement/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace E02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNumber = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            if (maxNumber == sum - maxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + maxNumber);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(maxNumber - (sum - maxNumber)));
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/F06. For-loops/2. Exercise/For-loops-Exercise/E03.OddorEvenPosition/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace E03.OddorEvenPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLoops = int.Parse(Console.ReadLine());
+            double oddSum = 0.00;
+            double oddMin = double.MaxValue;
+            double oddMax = double.MinValue;
+            double evenSum = 0.00;
+            double evenMin = double.MaxValue;
+            double evenMax = double.MinValue;
+            for (int i = 1; i <= numberOfLoops; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (i % 2 != 0)
+                {
+                    oddSum += number;
+                    if (number < oddMin)
+                    {
+                        oddMin = number;
+                    }
+
+                    if (number > oddMax)
+                    {
+                        oddMax = number;
+                    }
+                }
+
+                if (i % 2 == 0)
+                {
+                    evenSum += number;
+                    if (number < evenMin)
+                    {
+                        evenMin = number;
+                    }
+
+                    if (number > evenMax)
+                    {
+                        evenMax = number;
+                    }
+                }
+            }
+
+            Console.WriteLine($"OddSum={oddSum:F2},");
+            if (oddMin == double.MaxValue)
+            {
+                Console.WriteLine("OddMin=No,");
+            }
+
+            if (oddMax == double.MinValue)
+            {
+                Console.WriteLine("OddMax=No,");
+            }
+            else
+            {
+                Console.WriteLine($"OddMin={oddMin:F2},");
+                Console.WriteLine($"OddMax={oddMax:F2},");
+            }
+
+            Console.WriteLine($"EvenSum={evenSum:F2},");
+            if (evenMin == double.MaxValue)
+            {
+                Console.WriteLine("EvenMin=No,");
+            }
+
+            if (evenMax == double.MinValue)
+            {
+                Console.WriteLine("EvenMax=No");
+            }
+            else
+            {
+                Console.WriteLine($"EvenMin={evenMin:F2},");
+                Console.WriteLine($"EvenMax={evenMax:F2}");
+            }
+        }
+    }
+}
