5a021249c8a28a68ee9ab036b326487ec8603ba5
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 12:02:27 +0200

    Add files via upload

---
+++ Programming Basics with C#/F07. Nested loops/2. Exercise/Nested-loops-Exercise/E04.TraintheTrainers/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace E04.TraintheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countJury = int.Parse(Console.ReadLine());
+            string presentation = Console.ReadLine();
+            double sumAllGrades = 0;
+            int countAllGrades = 0;
+            while (presentation != "Finish")
+            {
+                double sum = 0;
+                for (int i = 1; i <= countJury; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sum += grade;
+                    sumAllGrades += grade;
+                    countAllGrades++;
+                }
+
+                double average = sum / countJury;
+                Console.WriteLine($"{presentation} - {average:F2}.");
+                presentation = Console.ReadLine();
+            }
+
+            double averageAllGrades = sumAllGrades / countAllGrades;
+            if (presentation == "Finish")
+            {
+                Console.WriteLine($"Student's final assessment is {averageAllGrades:F2}.");
+            }
+        }
+    }
+}
