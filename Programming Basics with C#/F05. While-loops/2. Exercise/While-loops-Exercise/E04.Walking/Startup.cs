c05a664c9ee524a9a49bb8252ccee54ad1033d35
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 10:49:32 +0200

    Add files via upload

---
+++ Programming Basics with C#/F05. While-loops/2. Exercise/While-loops-Exercise/E04.Walking/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace E04.Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 10000;
+            int walkedSteps = 0;
+            string steps = Console.ReadLine();
+            while (true)
+            {
+                if (steps == "Going home")
+                {
+                    steps = Console.ReadLine();
+                    walkedSteps += int.Parse(steps);
+                    break;
+                }
+                else
+                {
+                    walkedSteps += int.Parse(steps);
+                }
+
+                if (walkedSteps >= target)
+                {
+                    break;
+                }
+
+                steps = Console.ReadLine();
+            }
+
+            if (walkedSteps >= target)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+            }
+            else
+            {
+                Console.WriteLine($"{target - walkedSteps} more steps to reach goal.");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/F05. While-loops/2. Exercise/While-loops-Exercise/E06.Cake/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace E06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wight = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+
+            int areaCake = wight * lenght;
+            int areaOnePiece = 1;
+            int areaTotalPieces = 0;
+            int counterPieces = 0;
+            int pieces = 0;
+            bool isThereEnoughCake = true;
+
+            string input = Console.ReadLine();
+
+            while (input != "STOP")
+            {
+                pieces = int.Parse(input);
+                counterPieces += pieces;
+
+                areaTotalPieces = areaOnePiece * counterPieces;
+
+                if (areaCake < areaTotalPieces)
+                {
+                    int neededPieces = areaTotalPieces - areaCake;
+                    Console.WriteLine($"No more cake left! You need {neededPieces} pieces more.");
+                    isThereEnoughCake = false;
+                    break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (isThereEnoughCake == true)
+            {
+                int leftOver = areaCake - areaTotalPieces;
+                Console.WriteLine($"{leftOver} pieces are left.");
+            }
+        }
+    }
+}
