9edec4f227dff5e5ee0cfa7a12b76386d32ff2db
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 12:14:45 +0200

    Add files via upload

---
+++ Programming Basics with C#/F08. Drawing figures with loops/Drawing-Figures-with-Loops/E09.House/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace E09.House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int roof2 = 2;
+            int roof1 = 1;
+            for (int i = 0; i < (n + 1) / 2; i++)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("{0}{1}{0}",
+                       new string('-', ((n - 2) / 2) - i),
+                       new string('*', roof2)
+                       );
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{0}",
+                    new string('-', ((n - 1) / 2) - i),
+                    new string('*', roof1)
+                    );
+                }
+                roof2 += 2;
+                roof1 += 2;
+            }
+            roof2 -= 4;
+            roof1 -= 4;
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("|{0}|",
+                       new string('*', roof2)
+                       );
+                }
+                else
+                {
+                    Console.WriteLine("|{0}|",
+                    new string('*', roof1)
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/F08. Drawing figures with loops/Drawing-Figures-with-Loops/E10.Diamond/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace E10.Diamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int LeftRight = (n - 1) / 2;
+            int Middle = 0;
+            if (n % 2 == 0)
+            {
+                for (int i = 0; i < n / 2; i++)
+                {
+                    Console.WriteLine("{0}*{1}*{0}",
+                        new string('-', LeftRight),
+                        new string('-', Middle));
+                    LeftRight--;
+                    Middle += 2;
+                }
+
+                int LeftRightToDown = 1;
+                int MiddleDown = Middle - 4;
+                for (int i = 0; i < n / 2 - 1; i++)
+                {
+                    Console.WriteLine("{0}*{1}*{0}",
+                        new string('-', LeftRightToDown),
+                        new string('-', MiddleDown));
+                    LeftRightToDown++;
+                    MiddleDown -= 2;
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0}*{0}", new string('-', LeftRight));
+                LeftRight--;
+                Middle = 1;
+                for (int i = 0; i < (n / 2); i++)
+                {
+                    Console.WriteLine("{0}*{1}*{0}",
+                        new string('-', LeftRight),
+                        new string('-', Middle));
+                    LeftRight--;
+                    Middle += 2;
+                }
+
+                int LeftRight2Down = 1;
+                int MiddleDown = Middle - 4;
+                for (int i = 0; i < n / 2 - 1; i++)
+                {
+                    Console.WriteLine("{0}*{1}*{0}",
+                        new string('-', LeftRight2Down),
+                        new string('-', MiddleDown));
+                    LeftRight2Down++;
+                    MiddleDown -= 2;
+                }
+
+                if (n == 1)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("{0}*{0}", new string('-', (n - 1) / 2));
+                }
+            }
+        }
+    }
+}
