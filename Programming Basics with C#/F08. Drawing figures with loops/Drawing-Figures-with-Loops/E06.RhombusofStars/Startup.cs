67d5dafa78d9a92a68af3887726363534659e04a
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 12:05:35 +0200

    Add files via upload

---
+++ Programming Basics with C#/F08. Drawing figures with loops/Drawing-Figures-with-Loops/E06.RhombusofStars/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace E06.RhombusofStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            for (int row = 0; row < side; row++)
+            {
+                Console.Write(new string(' ', side - row - 1));
+                for (int numberOfSymbols = 0; numberOfSymbols <= row; numberOfSymbols++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int row = side - 1; row > 0; row--)
+            {
+                Console.Write(new string(' ', side - row));
+                for (int numberOfSymbols = row; numberOfSymbols > 0; numberOfSymbols--)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/F08. Drawing figures with loops/Drawing-Figures-with-Loops/E07.ChristmasTree/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace E07.ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int row = 0; row <= n; row++)
+            {
+                Console.Write(new string(' ', n - row));
+                Console.Write(new string('*', row));
+                Console.Write(" | ");
+                Console.Write(new string('*', row));
+                Console.WriteLine(new string(' ', n - row));
+            }
+        }
+    }
+}
