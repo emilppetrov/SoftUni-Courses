b6e97245a27dcf256dff6f5a7b5e436294d6e472
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 11:37:23 +0200

    Add files via upload

---
+++ Programming Basics with C#/F06. For-loops/1. Lab/For-loops-Lab/L04.EvenPowersof2/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace L04.EvenPowersof2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int power = 0; power <= n; power += 2)
+            {
+                if (power % 2 == 0)
+                {
+                    Console.WriteLine(Math.Pow(2, power));
+                }
+            }
+        }
+    }
+}
