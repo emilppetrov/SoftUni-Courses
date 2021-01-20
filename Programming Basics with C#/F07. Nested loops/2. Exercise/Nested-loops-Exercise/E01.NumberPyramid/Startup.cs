b8f85afb803466340cce21f8e67dcb859f5622da
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 11:42:53 +0200

    Add files via upload

---
+++ Programming Basics with C#/F07. Nested loops/2. Exercise/Nested-loops-Exercise/E01.NumberPyramid/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace E01.NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNumber = 1;
+            bool isBigger = false;
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int columns = 1; columns <= rows; columns++)
+                {
+                    if (currentNumber > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+
+                    Console.Write(currentNumber + " ");
+                    currentNumber++;
+                }
+
+                if (isBigger)
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
