612389086bf08f43c17e1006cf2577440c6a5ac6
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 11:53:48 +0200

    Add files via upload

---
+++ Programming Basics with C#/F06. For-loops/1. Lab/For-loops-Lab/L10.OddEvenSum/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace L10.OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += currentNumber;
+                }
+                else
+                {
+                    oddSum += currentNumber;
+                }
+            }
+
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {oddSum}");
+            }
+            else
+            {
+                int difference = Math.Abs(oddSum - evenSum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {difference}");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/F06. For-loops/1. Lab/For-loops-Lab/L11.CleverLily/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace L11.CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int singleToyPrice = int.Parse(Console.ReadLine());
+            double birthdayMoneySum = 0;
+            int evenBirthdaysCount = 0;
+            int toysCount = 0;
+
+            for (int birthdayNumber = 1; birthdayNumber <= age; birthdayNumber++)
+            {
+                if (birthdayNumber % 2 == 0)
+                {
+                    evenBirthdaysCount++;
+                    birthdayMoneySum += evenBirthdaysCount * 10;
+                }
+                else
+                {
+                    toysCount++;
+                }
+            }
+
+            int moneyFromSoldToys = toysCount * singleToyPrice;
+            int moneyClaimedFromBrother = evenBirthdaysCount * 1;
+            double resultingMoney = birthdayMoneySum + moneyFromSoldToys - moneyClaimedFromBrother;
+
+            if (resultingMoney >= washingMachinePrice)
+            {
+                double leftOverMoney = resultingMoney - washingMachinePrice;
+                Console.WriteLine($"Yes! {leftOverMoney:F2}");
+            }
+            else
+            {
+                double unsufficientMoney = washingMachinePrice - resultingMoney;
+                Console.WriteLine($"No! {unsufficientMoney:F2}");
+            }
+        }
+    }
+}
