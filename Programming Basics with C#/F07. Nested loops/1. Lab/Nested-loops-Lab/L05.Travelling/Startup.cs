61c1704caa9203b517eaf96625277bd6439a537a
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 12:00:24 +0200

    Add files via upload

---
+++ Programming Basics with C#/F07. Nested loops/1. Lab/Nested-loops-Lab/L05.Travelling/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace L05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End")
+                {
+                    break;
+                }
+
+                double expences = double.Parse(Console.ReadLine()), savings = 0;
+                while (savings < expences)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    savings += money;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/F07. Nested loops/1. Lab/Nested-loops-Lab/L06.Building/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace L06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floorsCount = int.Parse(Console.ReadLine());
+            int roomsPerFloorCount = int.Parse(Console.ReadLine());
+
+            for (int floorNumber = floorsCount; floorNumber >= 1; floorNumber--)
+            {
+                for (int roomNumber = 0; roomNumber < roomsPerFloorCount; roomNumber++)
+                {
+                    if (floorNumber == floorsCount)
+                    {
+                        Console.Write($"L{floorNumber}{roomNumber} ");
+                    }
+                    else if (floorNumber % 2 == 0)
+                    {
+                        Console.Write($"O{floorNumber}{roomNumber} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{floorNumber}{roomNumber} ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
