c3af203a65b2d27812cbad62ff7c7325e46bda2a
Emil Petrov <53703066+emilppetrov@users.noreply.github.com>
2021-01-20 10:44:33 +0200

    Add files via upload

---
+++ Programming Basics with C#/F05. While-loops/1. Lab/While-loops-Lab/L10.Moving/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace L10.Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeSpaceWidth = int.Parse(Console.ReadLine());
+            int freeSpaceLength = int.Parse(Console.ReadLine());
+            int freeSpaceHeight = int.Parse(Console.ReadLine());
+            int freeSpaceTotal = freeSpaceWidth * freeSpaceLength * freeSpaceHeight;
+            string input = Console.ReadLine();
+            while (input != "Done")
+            {
+                int boxes = int.Parse(input);
+                if (freeSpaceTotal < boxes)
+                {
+                    Console.WriteLine($"No more free space! You need {boxes - freeSpaceTotal} Cubic meters more.");
+                    break;
+                }
+
+                freeSpaceTotal -= boxes;
+                input = Console.ReadLine();
+            }
+
+            if (input == "Done")
+            {
+                Console.WriteLine($"{freeSpaceTotal} Cubic meters left.");
+            }
+        }
+    }
+}
